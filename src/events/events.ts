ee9c9881f3246669f632c1d61704e046e6dad3d6
Nate <nathanh722@gmail.com>
2019-05-29 12:22:25 -0700

    Continue work on event system

--- src/events/events.ts
+++ src/events/events.ts
@@ -1,6 +1,4 @@
-
-
-export type EventFunction = (args: any[]) => void
+export type EventFunction = (args?: any[]) => void
 
 export class Event {
   name: string
@@ -19,15 +17,27 @@ export class Event {
     if (onTrigger) {
       this.onTrigger = onTrigger
     } else {
-      this.onTrigger = (args: any[]) => {
+      this.onTrigger = (args?: any[]) => {
         return undefined
       }
     }
   }
-}
 
-export let Events = {
-  registerEvent: function (event: Event, eventList?: string) {
+  // Static Methods
+  public static registerEvent (event: Event, eventList: string = "main"): void {
+    Events[eventList][event.name] = event
+  }
 
+  public static trigger(event: String | Event, eventList: string = "main") {
+    if (typeof event == "string") {
+      Events[eventList][event].event()
+    } else if (event instanceof Event) {
+      event.event()
+    }
   }
 }
+
+type EventList = {[name: string]: Event}
+type EventsHolder = {[name: string]: EventList}
+
+export let Events: EventsHolder = {}
