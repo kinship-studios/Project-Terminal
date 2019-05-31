61b3c068bbafeaec22745fb4a4ffa774b6bd64f6
Nate <nathanh722@gmail.com>
2019-05-30 22:29:59 -0700

    Add color.ts from Palette; work on colortext

--- src/util/colortext.ts
+++ src/util/colortext.ts
@@ -1,2 +1,27 @@
+import { Color } from './color';
 
-export class ColoredText
+type ColorSet = {[name: string]: Color}
+
+export class ColoredText {
+  input: string
+  output: string
+
+  colorSet: ColorSet = {
+    red: Color.red,
+    yellow: Color.yellow,
+    green: Color.green,
+    blue: Color.blue,
+    pink: Color.pink,
+    purple: Color.purple,
+
+    white: Color.white,
+    gray: Color.gray,
+    black: Color.black
+  }
+}
+
+export module ColoredText {
+  export function parse (input: string, colorSet?: ColorSet) {
+
+  }
+}
